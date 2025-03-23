96bbb38035b2095c9c047a19f3a587cb30f7a9e3
zegenerative <zeger@mywheels.nl>
2025-03-23 08:20:35 +0100

    Fix typo

--- dist/calculateTotalWorkloadDuration.js
+++ dist/calculateTotalWorkloadDuration.js
@@ -6,7 +6,7 @@ const calculateDuration_1 = require("./calculateDuration");
 /**
  * Calculates how much minutes it takes to complete all tasks per moped
  * @param workload list of mopeds and distances between them
- * @returns total distance duration in minutes
+ * @returns total workload duration in minutes
  */
 const calculateTotalWorkloadDuration = (workload, employees) => {
     const totalWorkDuration = Object.values(employees).reduce((acc, curr) => {
